0c2f11f0c4b06a7fa9a1fc291f4f504a16018829
Kcovva <diana.menchinska@gmail.com>
2025-10-21 01:12:37 +0300

    feat: add division and remainder operations
    Signed-off-by: Kcovva <diana.menchinska@gmail.com>

--- calculator.h
+++ calculator.h
@@ -7,6 +7,8 @@ class Calculator
         int Add (double, double);
         int Sub (double, double);
         int Mul (double, double);
+        int Div (double, double);
+	int Remainder (double a, double b);
 };
 
 #endif//CALCULATOR_H
